02c626b2f55169ee902efcd088ac752e9e0201ee
Dushyant Yadav <dushyant9309@gmail.com>
2023-05-15 17:23:39 +0530

    yee

--- inject/inject.ts
+++ inject/inject.ts
@@ -15,6 +15,7 @@ class Inject extends Symbol {
     onInit: Symbol['onInit'] = async (sendMessage) => {
         this.runtime.dynamicRouter.post(`/inject/${this.id}`, (ctx) => {
             const payload = Inject.schema.propertiesSchema.payload.evaluateField(this, 'payload', {})
+            console.log('Received message:', ctx.request.body())
             const msg = {
                 _id: Date.now().toString(36),
                 payload: payload,
